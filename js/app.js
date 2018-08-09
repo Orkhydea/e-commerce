9851e8f206c5311789eccc740fd05148a38abc45
Orquídea Campos <37587788+Orkhydea@users.noreply.github.com>
2018-08-08 19:31:03 -0500

    imagen

--- js/app.js
+++ js/app.js
@@ -6,38 +6,39 @@ $(document).ready(function () {
     //   });
 });
 
-var showHogar = function(hogares) {
+var showHogar = function (hogares) {
     var name = "";
     var imagen = "";
-  
 
-  hogares.results.forEach(function (hogar) {
-      console.log(hogar); 
-      name=hogar.title;
-      imagen=hogar.thumbnail;
-      $("#elementos").append(armarTemplate(name,imagen))
-  })
+
+    hogares.results.forEach(function (hogar) {
+        console.log(hogar);
+        name = hogar.title;
+        imagen = hogar.thumbnail;
+        precio = hogar.price;
+        $("#elementos").append(armarTemplate(name, imagen))
+    })
 }
 
-var armarTemplate = function (name, imagen) {
-  var t = "<div class='elemento'><p>" + name + "</p><img src='" + imagen + "' width='150' heigth='150' /></div>";
-  return t;
+var armarTemplate = function (name, imagen, price) {
+    var t = "<div class='card' style='width: 15rem;'><img class='card-img-top' src='" + imagen + "alt='Card image cap'><div class='card-body'><h5 class='card-title'>" + name + "</h5><p class='card-text'><i class='fas fa-dollar-sign'></i> " + precio + "</p><a href='#' class='btn btn-primary'>Go somewhere</a></div></div>";
+    return t;
 }
 
 var ajaxHogar = function () {
-  $.ajax({
-      url: `https://api.mercadolibre.com/sites/MLM/search?category=MLM1574`,
-      type: 'GET',
-      datatype: 'json',
-      //   data: {
-      //       q : hogar,
-      //   }
-  })
-      .done(function (response) {
-          console.log(response);
-          showHogar(response);
-      })
-      .fail(function (xhr) {
-          console.log("error");
-      });
+    $.ajax({
+        url: `https://api.mercadolibre.com/sites/MLM/search?category=MLM1574`,
+        type: 'GET',
+        datatype: 'json',
+        //   data: {
+        //       q : hogar,
+        //   }
+    })
+        .done(function (response) {
+            console.log(response);
+            showHogar(response);
+        })
+        .fail(function (xhr) {
+            console.log("error");
+        });
 }
